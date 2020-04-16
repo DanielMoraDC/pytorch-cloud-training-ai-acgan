34882807be93ecba1aa6d5ce43aae3e5c5659c3e
Daniel <daniel.mora.checa@gmail.com>
2020-04-16 20:18:02 +0200

    Refactor code
    By doing so, we can make simpler the launching script.

---
+++ acgan/model.py
@@ -0,0 +1,252 @@
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+import torch
+
+from torch.utils.tensorboard import SummaryWriter
+
+from acgan.discriminator import (
+    Discriminator,
+    initialize_weights as init_weights_d
+)
+
+from acgan.generator import (
+    Generator,
+    initialize_weights as init_weights_g
+)
+
+
+DEFAULT_DEVICE = torch.device("cuda:0") if torch.cuda.is_available() else 'cpu'
+CUDA_ENABLED = True if torch.cuda.is_available() else False
+
+
+class ACGAN(object):
+
+    # Adapted from https://github.com/clvrai/ACGAN-PyTorch/blob/master/main.py
+
+    def __init__(self,
+                 dataset: DataLoader,
+                 image_size: int,
+                 n_channels: int,
+                 n_classes: int,
+                 latent_dimension: int,
+                 g_base_maps: int = 64,
+                 d_base_maps: int = 64):
+        self._data_loader = dataset
+        self._image_size = image_size
+        self._n_channels = n_channels
+        self._n_classes = n_classes
+        self._latent_dimension = latent_dimension
+        self._g_base_maps = g_base_maps
+        self._d_base_maps = d_base_maps
+
+        self._discriminator = self._build_discriminator()
+        self._generator = self._build_generator()
+
+    def _build_generator(self):
+        g = Generator(n_classes=self._n_classes,
+                      n_channels=self._n_channels,
+                      img_size=self._image_size,
+                      latent_dimensions=self._latent_dimension,
+                      base_feature_maps=self._g_base_maps)
+
+        if CUDA_ENABLED:
+            g.cuda()
+
+        return g
+
+    def _build_discriminator(self):
+        d = Discriminator(n_classes=self._n_classes,
+                          n_channels=self._n_channels,
+                          img_size=self._image_size,
+                          base_feature_maps=self._d_base_maps)
+        if CUDA_ENABLED:
+            d.cuda()
+
+        return d
+
+    @staticmethod
+    def _losses():
+        adversarial_loss = torch.nn.BCELoss()
+        auxiliary_loss = torch.nn.CrossEntropyLoss()
+
+        if CUDA_ENABLED:
+            adversarial_loss.cuda()
+            auxiliary_loss.cuda()
+
+        return adversarial_loss, auxiliary_loss
+
+    def _optimizers(self, lr: float, b1: float, b2: float):
+        optimizer_g = torch.optim.Adam(self._generator.parameters(),
+                                       lr=lr,
+                                       betas=(b1, b2))
+        optimizer_d = torch.optim.Adam(self._discriminator.parameters(),
+                                       lr=lr,
+                                       betas=(b1, b2))
+        return optimizer_g, optimizer_d
+
+    def _train_generator(self,
+                         batch_size,
+                         optimizer,
+                         adversarial_loss,
+                         auxiliary_loss):
+        # Make sure we do not accumulate gradients
+        optimizer.zero_grad()
+
+        # Sample noise and labels as generator input
+        z = torch.randn(batch_size, self._latent_dimension).to(DEFAULT_DEVICE)
+        generated_labels = torch.randint(
+            low=0, high=self._n_classes, size=(batch_size,)).to(DEFAULT_DEVICE)
+
+        # Generate a batch of images
+        generated_images = self._generator(z, generated_labels)
+
+        # Loss measures generator's ability to fool the discriminator
+        validity, pred_label = self._discriminator(generated_images)
+        valid = torch.full(
+            size=(batch_size, 1), fill_value=1.0).to(DEFAULT_DEVICE)
+        loss = 0.5 * (adversarial_loss(validity, valid) + auxiliary_loss(pred_label, generated_labels))  # noqa
+
+        # Compute loss and apply gradients
+        loss.backward()
+        optimizer.step()
+
+        return loss, generated_images, generated_labels
+
+    def _train_discriminator(self,
+                             images,
+                             labels,
+                             generated_images,
+                             generated_labels,
+                             optimizer,
+                             adversarial_loss,
+                             auxiliary_loss):
+        batch_size = images.shape[0]
+        optimizer.zero_grad()
+
+        # Loss for real images
+        real_pred, real_class = self._discriminator(images)
+        valid = torch.full(
+            size=(batch_size, 1), fill_value=1.0).to(DEFAULT_DEVICE)
+        real_loss = (adversarial_loss(real_pred, valid) + auxiliary_loss(real_class, labels)) / 2  # noqa
+
+        # Loss for fake images
+        fake_pred, fake_class = self._discriminator(generated_images.detach())
+        fake = torch.full(
+            size=(batch_size, 1), fill_value=0.0).to(DEFAULT_DEVICE)
+        fake_loss = (adversarial_loss(fake_pred, fake) + auxiliary_loss(fake_class, generated_labels)) / 2  # noqa
+
+        # Total discriminator loss
+        loss = (real_loss + fake_loss) / 2
+
+        # Calculate discriminator accuracy
+        predicted = np.concatenate(
+            [real_class.data.cpu().numpy(), fake_class.data.cpu().numpy()],
+            axis=0
+        )
+        groundtruth = np.concatenate(
+            [labels.data.cpu().numpy(), generated_labels.data.cpu().numpy()],
+            axis=0
+        )
+        accuracy = np.mean(np.argmax(predicted, axis=1) == groundtruth)
+
+        # Compute loss and apply gradients
+        loss.backward()
+        optimizer.step()
+
+        return loss, real_loss, fake_loss, accuracy
+
+    @staticmethod
+    def _track_metrics(writer,
+                       g_loss,
+                       d_loss,
+                       d_real_loss,
+                       d_fake_loss,
+                       acc,
+                       iteration):
+        # Write scalar stats
+        writer.add_scalar('Discriminator loss', d_loss.item(), iteration)
+        writer.add_scalar(
+            'Discriminator real label loss', d_real_loss.item(), iteration)
+        writer.add_scalar(
+            'Discriminator fake label loss', d_fake_loss.item(), iteration)
+        writer.add_scalar('Generator loss', g_loss.item(), iteration)
+        writer.add_scalar(
+            'Discriminator accuracy', acc.item(), iteration)
+
+    def _track_generated_images(self, writer, iteration):
+        samples_per_class = 10
+        # Generated hidden vector
+        z = torch.randn(
+            samples_per_class * self._n_classes, self._latent_dimension)
+        z = z.to(DEFAULT_DEVICE)
+        # Generate fake labels
+        labels = np.array([
+            num for num in range(samples_per_class) for _ in
+            range(self._n_classes)])
+        labels = torch.Tensor(labels).to(DEFAULT_DEVICE)
+        # Store images
+        gen_imgs = self._generator(z, labels)
+        writer.add_images('gan_grid', gen_imgs, iteration)
+
+    def train(self,
+              n_epochs: int,
+              logs_dir: str,
+              lr: float,
+              b1: float,
+              b2: float,
+              stats_interval: int):
+        self._generator.apply(init_weights_g)
+        self._discriminator.apply(init_weights_d)
+
+        adversarial_loss, auxiliary_loss = ACGAN._losses()
+
+        optimizer_g, optimizer_d = self._optimizers(lr, b1, b2)
+
+        batches_per_epoch = len(self._data_loader)
+        total_batches = len(self._data_loader)
+
+        writer = SummaryWriter(logs_dir)
+
+        for epoch in range(n_epochs):
+            for i, (imgs, labels) in enumerate(self._data_loader):
+
+                real_imgs = imgs.to(DEFAULT_DEVICE)
+                real_labels = labels.to(DEFAULT_DEVICE)
+
+                g_loss, gen_images, gen_labels = \
+                        self._train_generator(batch_size=imgs.shape[0],
+                                              optimizer=optimizer_g,
+                                              adversarial_loss=adversarial_loss,  # noqa
+                                              auxiliary_loss=auxiliary_loss)
+
+                d_loss, d_real_loss, d_fake_loss, acc = \
+                    self._train_discriminator(images=real_imgs,
+                                              labels=real_labels,
+                                              generated_images=gen_images,
+                                              generated_labels=gen_labels,
+                                              optimizer=optimizer_d,
+                                              adversarial_loss=adversarial_loss,  # noqa
+                                              auxiliary_loss=auxiliary_loss)
+
+                iteration = epoch * batches_per_epoch + i
+
+                if iteration % stats_interval == 0:
+                    ACGAN._track_metrics(
+                        writer, g_loss, d_loss, d_real_loss, d_fake_loss, acc, iteration)  # noqa
+                    self._track_generated_images(writer, iteration)
+
+                iteration_info = "[Epoch %d/%d] [Batch %d/%d]" \
+                                 % (epoch, n_epochs, i, total_batches)
+                metrics_info = "[D loss: %f, acc: %d%%] [G loss: %f]" \
+                               % (d_loss.item(), 100 * acc, g_loss.item())
+                print(" ".join([iteration_info, metrics_info]))
+
+        writer.close()
+
+    def save_discriminator(self, dst: str):
+        torch.save(self._discriminator, dst)
+
+    def save_generator(self, dst):
+        torch.save(self._generator,  dst)
--- acgan/train.py
+++ acgan/train.py
@@ -1,24 +1,10 @@
 import argparse
 import os
-import numpy as np
-
-import torch
-from torch.utils.tensorboard import SummaryWriter
 
 from google.cloud import storage
 
-from acgan.discriminator import (
-    Discriminator,
-    initialize_weights as init_weights_d
-)
-from acgan.generator import (
-    Generator,
-    initialize_weights as init_weights_g
-)
 from acgan.data import fashion_mnist
-
-# ACGAN
-# Adapted from https://github.com/clvrai/ACGAN-PyTorch/blob/master/main.py
+from acgan.model import ACGAN
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--job-dir",
@@ -69,59 +55,15 @@ parser.add_argument("--stats_interval",
                     type=int,
                     default=100,
                     help="Interval between Tensorboard metrics tracking")
-parser.add_argument("--sample_interval",
-                    type=int,
-                    default=400,
-                    help="Interval between image sampling during training")
+
 opt = parser.parse_args()
 print(f'Run parameters: {opt}')
 
-n_classes = 10
-n_channels = 1
-
-device = torch.device("cuda:0") if torch.cuda.is_available() else 'cpu'
-cuda = True if torch.cuda.is_available() else False
-
-# Loss functions
-adversarial_loss = torch.nn.BCELoss()
-auxiliary_loss = torch.nn.CrossEntropyLoss()
-
-# Initialize generator and discriminator
-generator = Generator(n_classes=n_classes,
-                      n_channels=1,
-                      img_size=opt.img_size,
-                      latent_dimensions=opt.latent_dim,
-                      base_feature_maps=64)
-
-discriminator = Discriminator(n_classes=n_classes,
-                              n_channels=1,
-                              img_size=opt.img_size,
-                              base_feature_maps=16)
-
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-    adversarial_loss.cuda()
-    auxiliary_loss.cuda()
-
-# Initialize weights
-generator.apply(init_weights_g)
-discriminator.apply(init_weights_d)
-
 # Configure data loader
 os.makedirs(opt.data_dir, exist_ok=True)
-dataloader = fashion_mnist(opt.data_dir,
-                           img_size=opt.img_size,
-                           batch_size=opt.batch_size)
-batches_per_epoch = len(dataloader)
-
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(),
-                               lr=opt.lr,
-                               betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(),
-                               lr=opt.lr,
-                               betas=(opt.b1, opt.b2))
+data_loader = fashion_mnist(opt.data_dir,
+                            img_size=opt.img_size,
+                            batch_size=opt.batch_size)
 
 # ------------
 # Google Cloud storage configuration
@@ -137,113 +79,27 @@ def upload_file(src: str):
     blob = bucket.blob(os.path.basename(src))
     blob.upload_from_filename(src)
 
-# ------------
-# Tensorboard configuration
-# ------------
-
-
-writer = SummaryWriter(opt.job_dir)
-
 # ----------
 #  Training
 # ----------
 
 
-def sample_image(writer: SummaryWriter,
-                 samples_per_class: int,
-                 iterations: int):
-    z = torch.randn(samples_per_class * n_classes, opt.latent_dim).to(device)
-    labels = np.array([
-        num for num in range(samples_per_class) for _ in range(n_classes)])
-    labels = torch.Tensor(labels).to(device)
-    gen_imgs = generator(z, labels)
-    writer.add_images('gan_grid', gen_imgs, iterations)
+model = ACGAN(data_loader,
+              image_size=opt.img_size,
+              n_channels=1,
+              n_classes=10,
+              latent_dimension=opt.latent_dim)
 
+model.train(n_epochs=opt.n_epochs,
+            logs_dir=opt.job_dir,
+            lr=opt.lr,
+            b1=opt.b1,
+            b2=opt.b2,
+            stats_interval=opt.stats_interval)
 
-for epoch in range(opt.n_epochs):
-    for i, (imgs, labels) in enumerate(dataloader):
-
-        iteration = epoch * batches_per_epoch + i
-        batch_size = imgs.shape[0]
-
-        # Adversarial ground truths
-        valid = torch.full(size=(batch_size, 1), fill_value=1.0).to(device)
-        fake = torch.full(size=(batch_size, 1), fill_value=0.0).to(device)
-
-        # Configure batch data
-        real_imgs = imgs.to(device)
-        labels = labels.to(device)
-
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        optimizer_G.zero_grad()
-
-        # Sample noise and labels as generator input
-        z = torch.randn(batch_size, opt.latent_dim).to(device)
-        gen_labels = torch.randint(
-            low=0, high=n_classes, size=(batch_size,)).to(device)
-
-        # Generate a batch of images
-        gen_imgs = generator(z, gen_labels)
-
-        # Loss measures generator's ability to fool the discriminator
-        validity, pred_label = discriminator(gen_imgs)
-        g_loss = 0.5 * (adversarial_loss(validity, valid) + auxiliary_loss(pred_label, gen_labels))
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Loss for real images
-        real_pred, real_aux = discriminator(real_imgs)
-        d_real_loss = (adversarial_loss(real_pred, valid) + auxiliary_loss(real_aux, labels)) / 2
-
-        # Loss for fake images
-        fake_pred, fake_aux = discriminator(gen_imgs.detach())
-        d_fake_loss = (adversarial_loss(fake_pred, fake) + auxiliary_loss(fake_aux, gen_labels)) / 2
-
-        # Total discriminator loss
-        d_loss = (d_real_loss + d_fake_loss) / 2
-
-        # Calculate discriminator accuracy
-        pred = np.concatenate([real_aux.data.cpu().numpy(), fake_aux.data.cpu().numpy()], axis=0)
-        gt = np.concatenate([labels.data.cpu().numpy(), gen_labels.data.cpu().numpy()], axis=0)
-        d_acc = np.mean(np.argmax(pred, axis=1) == gt)
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        if iteration % opt.stats_interval == 0:
-            writer.add_scalar('Discriminator loss', d_loss.item(), iteration)
-            writer.add_scalar(
-                'Discriminator real label loss', d_real_loss.item(), iteration)
-            writer.add_scalar(
-                'Discriminator fake label loss', d_real_loss.item(), iteration)
-            writer.add_scalar('Generator loss', g_loss.item(), iteration)
-            writer.add_scalar(
-                'Discriminator accuracy', d_acc.item(), iteration)
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %d%%] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), 100 * d_acc, g_loss.item())
-        )
-
-        if iteration % opt.sample_interval == 0:
-            sample_image(writer, samples_per_class=10, iterations=iteration)
-
-# Save resulting model, locally
-torch.save(discriminator, 'discriminator.pt')
-torch.save(generator,  'generator.pt')
-
-# Update results to GCP
+# Update models to GCP
+model.save_discriminator('discriminator.pt')
 upload_file('discriminator.pt')
-upload_file('generator.pt')
 
-writer.close()
+model.save_generator('generator.pt')
+upload_file('generator.pt')
